5ce4e0608373b6c159d297af5cca23029737c905
Pete Whelpton <peedeeboy@gmail.com>
2026-02-14 21:12:18 +0000

    Frontend: Basic task edit view

--- frontend/src/main/routes/home.ts
+++ frontend/src/main/routes/home.ts
@@ -4,12 +4,12 @@ import axios from 'axios';
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
-      const response = await axios.get('http://localhost:4000/api/tasks');
-      console.log(response.data);
-      res.render('home', { "tasks": response.data });
+      const tasks = await axios.get('http://localhost:4000/api/tasks');
+      console.log(tasks.data);
+      res.render('home', { "tasks": tasks.data });
     } catch (error) {
       console.error('Error making request:', error);
-      res.render('home', {});
+      res.render('error', {});
     }
   });
   app.get('/task/add', async (req, res) => {
@@ -33,11 +33,49 @@ export default function (app: Application): void {
               dueDate: dueDate
             }
          );
-        res.render('add-task');
+        res.redirect('/');
      } catch (error) {
-             console.error('Error making request:', error);
-             res.render('home', {});
+       console.error('Error making request:', error);
+       res.render('error', {});
      }
+  });
+  app.get('/task/:id', async (req, res) => {
+    try {
+        const taskId = req.params.id
+        const task = await axios.get(`http://localhost:4000/api/tasks/${taskId}`);
+        console.log(task.data)
+        res.render('edit-task', { "task": task.data });
+    } catch (error) {
+        console.error('Error making request:', error);
+        res.render('error', {});
+    }
+  });
+  app.post('/task/:id', async (req, res) => {
+    try {
+        console.log(req.body)
+        const taskId = req.params.id
+        const title = req.body['title']
+        const description = req.body['description']
+        const status = req.body['status']
+        const dueDateDay = req.body['due-date-day']
+        const dueDateMonth = req.body['due-date-month']
+        const dueDateYear = req.body['due-date-year']
+
+        const dueDate = `${dueDateYear}-${dueDateMonth}-${dueDateDay}`
+        axios.put(`http://localhost:4000/api/tasks/${taskId}`,
+            {
+              id: taskId,
+              title: title,
+              description: description,
+              status: status,
+              dueDate: dueDate
+            }
+        );
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error making request:', error);
+        res.render('error', {});
+    }
   });
   app.get('*', function(req, res){
     res.status(404).render('not-found');
