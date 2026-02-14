7cc8d257d0d78f335c7f4a89459eb9a57b8220b3
Pete Whelpton <peedeeboy@gmail.com>
2026-02-14 15:19:57 +0000

    Frontend: Basic task list

--- frontend/src/main/routes/home.ts
+++ frontend/src/main/routes/home.ts
@@ -4,10 +4,9 @@ import axios from 'axios';
 export default function (app: Application): void {
   app.get('/', async (req, res) => {
     try {
-      // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/get-example-case');
+      const response = await axios.get('http://localhost:4000/api/tasks');
       console.log(response.data);
-      res.render('home', { "example": response.data });
+      res.render('home', { "tasks": response.data });
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
