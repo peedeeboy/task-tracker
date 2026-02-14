a69981f80e76c4feab007c688760c37866a47738
Pete Whelpton <peedeeboy@gmail.com>
2026-02-14 20:04:13 +0000

    Frontend: Basic Add Task view

--- frontend/src/main/routes/home.ts
+++ frontend/src/main/routes/home.ts
@@ -12,4 +12,34 @@ export default function (app: Application): void {
       res.render('home', {});
     }
   });
+  app.get('/task/add', async (req, res) => {
+     res.render('add-task');
+  });
+  app.post('/task/add', async (req, res) => {
+     try {
+         console.log(req.body)
+         const title = req.body['title']
+         const description = req.body['description']
+         const dueDateDay = req.body['due-date-day']
+         const dueDateMonth = req.body['due-date-month']
+         const dueDateYear = req.body['due-date-year']
+
+         const dueDate = `${dueDateYear}-${dueDateMonth}-${dueDateDay}`
+         axios.post('http://localhost:4000/api/tasks',
+            {
+              title: title,
+              description: description,
+              status: 'Not started',
+              dueDate: dueDate
+            }
+         );
+        res.render('add-task');
+     } catch (error) {
+             console.error('Error making request:', error);
+             res.render('home', {});
+     }
+  });
+  app.get('*', function(req, res){
+    res.status(404).render('not-found');
+  });
 }
